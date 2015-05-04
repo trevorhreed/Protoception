cdacff6bb9e97ab6b0f182a037c4ddafe15cde4b
Trevor Reed <trevor.reed@byu.edu>
2015-05-04 15:50:39 -0600

    adding initial content (first draft)

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+var gulp        = require('gulp'),
+    gutil       = require('gulp-util'),
+    nghtml      = require('gulp-angular-templatecache'),
+    sass        = require('gulp-sass'),
+    concat      = require('gulp-concat'),
+    watch       = require('gulp-watch'),
+    addsrc      = require('gulp-add-src')
+    nodemon     = require('gulp-nodemon'),
+    plumber     = require("gulp-plumber")
+    tinylr      = require('tiny-lr')(),
+    path        = require('path')
+;
+
+gulp.origSrc = gulp.src;
+gulp.src = function () {
+	var origin = arguments.length > 0 ? arguments[0] : "Unknown";
+	return gulp
+		.origSrc
+		.apply(gulp, arguments)
+		.pipe(plumber({
+			errorHandler: function (err) {
+				gutil.beep();
+				gutil.log("");
+				gutil.log("");
+				gutil.log("      - Error (" + origin + "): ", err.message);
+				gutil.log("");
+				gutil.log("");
+				this.emit("end");
+			}
+		}));
+}
+function end(msg) {
+	return function () {
+		gutil.log("      - recompiled (" + msg + ").");
+	}
+}
+
+var TINYLR_PORT     = 35729,
+    SRC_ROOT        = './app/',
+    SCSS_GLOB       = SRC_ROOT + '**/*.scss',
+    HTML_GLOB       = SRC_ROOT + '**/*.html',
+    CSS_GLOB        = SRC_ROOT + '**/*.css',
+    JS_GLOB         = SRC_ROOT + '**/*.js',
+
+    DST_ROOT        = './static/',
+    DST_JS          = 'app.js',
+    DST_CSS         = 'app.css',
+    DST_ASSETS      = [DST_ROOT + DST_JS, DST_ROOT + DST_CSS]
+
+    NG_HTML_OPTIONS = {
+        "module": "app",
+        "base": function (file){
+          var name = file.path,
+              start = name.lastIndexOf(path.sep) + 1,
+              end = name.lastIndexOf('.');
+          if(end < 0) end = file.path.length - 1;
+          return name.substring(start, end);
+        }
+    }
+;
+
+function compileSass(){
+  return gulp.src(SCSS_GLOB)
+    .pipe(sass())
+    .pipe(concat(DST_CSS))
+    .pipe(gulp.dest(DST_ROOT))
+    .on('end', end('css'));
+}
+function compileJs(){
+  return gulp
+    .src(HTML_GLOB)
+    .pipe(nghtml(NG_HTML_OPTIONS))
+    .pipe(addsrc(JS_GLOB))
+    .pipe(concat(DST_JS))
+    .pipe(gulp.dest(DST_ROOT))
+    .on('end', end('js'));
+}
+function reload(event){
+  var filename = path.relative(__dirname + SRC_ROOT, event.path);
+  tinylr.changed({ body: { files: [filename] } });
+}
+
+gulp.task('build', function(){
+  compileSass();
+  compileJs();
+});
+
+gulp.task('default', ['build'], function(){
+    tinylr.listen(TINYLR_PORT);
+    nodemon({script: 'server.js'});
+    watch(SCSS_GLOB, compileSass);
+    watch([HTML_GLOB, JS_GLOB], compileJs);
+    gulp.watch([HTML_GLOB, CSS_GLOB, JS_GLOB], reload);
+})
---
+++ server.js
@@ -0,0 +1,83 @@
+var express = require('express'),
+    connectlr = require('connect-livereload'),
+    sanitize = require('sanitize-filename'),
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    path = require('path'),
+    spahql = require('spahql'),
+    uuid = require('node-uuid'),
+    urljoin = require('url-join')
+;
+
+function handle(method){
+  return function(req, res){
+    var file = path.join(__dirname, 'data.json')
+    fs.readFile(file, function(err, data){
+
+      if(err){
+        console.log(err);
+        res.status(500).end();
+        return;
+      }
+
+      try {
+
+        var url = req.path.substr(5),
+            db = spahql.db(JSON.parse(data)),
+            node = db.select(url);
+            value = method(node, url, db, req.body);
+
+        json = JSON.stringify(db.sourceData(), null, 2);
+        fs.writeFile(file, json);
+        res.json(value);
+
+      } catch(ex) {
+        console.log(ex);
+        res.status(500).end();
+        return;
+      }
+
+    });
+  }
+}
+
+function get(node, url, db, entity){
+  return node.value();
+}
+function put(node, url, db, entity){
+  var index = url.lastIndexOf('/') + 1,
+      location = url.substr(0, index),
+      key = url.substr(index);
+
+  if(!db.assert(location)){
+    var nodes = url.split('/'),
+        prev = '/';
+    for(var i=0; i < nodes.length; i++){
+      var node = nodes[i],
+          next = urljoin(prev, node);
+      if(!db.assert(next)){
+        db.select(prev).set(node, {});
+      }
+      prev = next;
+    }
+  }
+
+  return db.select(location).set(key, entity);
+}
+function post(node, url, db, entity){
+  var id = uuid.v4();
+  return node.set(id, entity);
+}
+function del(node, url, db, entity){
+  return node.destroy(undefined);
+}
+
+express()
+    .use(bodyParser.json())
+    .get('/data/*', handle(get))
+    .put('/data/*', handle(put))
+    .post('/data/*', handle(post))
+    .delete('/data/*', handle(del))
+    .use(connectlr())
+    .use(express.static('static'))
+    .listen(3000);
